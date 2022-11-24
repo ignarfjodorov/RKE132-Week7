611745464976589c34c255dcfb8c8ffee2088190
ignarfjodorov <ignar.fjodorov@gmail.com>
2022-11-24 21:31:29 +0200

    add week7 code

---
+++ Week7Functions/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Are you coming or leaving?");
+string userChoise = Console.ReadLine();
+
+if(userChoise == "coming")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodBye();
+}
+
+
+
+
+
+static void PrintHello() //function / method
+{
+    Console.WriteLine("hello, world!");
+}
+
+static void PrintGoodBye()
+{
+    Console.WriteLine("See you later.");
+}
+
